3d217c800d8e0ad3d2201f595a29e846f4264f10
jonas costa <costajonas520@gmail.com>
2024-08-12 21:36:53 -0300

    fix:base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/certificado-online",
-  base: "/",
+  base: "/certificado-online",
 });
